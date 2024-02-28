45c4e57e073a4087dd0bbc660bdfd79761f475a4
illusionyes <87438374+illusionyes@users.noreply.github.com>
2024-02-28 22:06:21 +0300

    AddMenuScene

---
+++ Assets/Scripts/Ui/MenuUi.cs
@@ -0,0 +1,29 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUi : MonoBehaviour
+{
+    private bool isChoose;
+    public void LoadMainScene()
+    {
+        if (isChoose)
+        {
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            Debug.Log("Need choose hero!");
+        }
+    }
+
+    public void Exit()
+    {
+        EditorApplication.ExitPlaymode();
+    }
+
+    public void ChooseHero(int value)
+    {
+        isChoose = true;
+    }
+}
